95c42d83c1e588d6b976c3da82692ea4f1f63757
Haydn Johnson <haydn.johnson@toronto.exclamation.com>
2020-02-20 16:23:07 -0500

    work on  25-30 format

--- src/portstrings-parsing.go
+++ src/portstrings-parsing.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	//"strconv"
 	"strings"
 )
 
 func main() {
 
-	portStrings := "80,443,8080"
+	portStrings := "80,443,8080,25-30"
 
 	//removeQuotes := strings.Replace(portStrings, "\"", " ", -1)
 
@@ -15,7 +16,25 @@ func main() {
 	fmt.Printf("%q\n", portStrings)
 
 	for _, rune := range splitString {
-		fmt.Printf("test:%s80\n", rune)
+		if rune == "25-30" { // need to add IF "-" in string
+			fmt.Println("TEST")
+			rune2 := strings.Split(rune, "-") //[25,30]
+
+			for _, rune := range rune2 {
+				// I DONT NEED TO CONVERT TO INT LOL
+				//intParsed, err := strconv.Atoi(rune)
+				//if err == nil {
+				//fmt.Println("%s", intParsed)
+				//fmt.Printf("test:%d\n", intParsed)
+				fmt.Printf("test:%s\n", rune)
+
+			}
+			//fmt.Println(intParsed)
+			//fmt.Println(strings.Split(rune, "-"))
+
+		} else {
+			fmt.Printf("test:%s\n", rune)
+		}
 	}
 
 }
