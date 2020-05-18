2e36bbf40ffa18a6f9de354a9e4cea501902bb4d
Haydz <haydnit@gmail.com>
2020-05-18 18:19:02 -0400

    go tour, and researching http client

---
+++ src/Blog/httpclients.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// References:
+/*
+https://golang.org/pkg/io/ioutil/
+Package ioutil implements some I/O utility functions.
+
+ReadALL
+uses https://golang.org/pkg/io/#Reader a reader
+
+
+https://golang.org/pkg/os/#FileInfo
+from within ReadDir
+
+*/
+
+func main() {
+
+	client := &http.Client{
+		//CheckRedirect: redirectPolicyFunc,
+	}
+
+	resp, err := client.Get("http://example.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(body)
+	fmt.Printf("%s", body)
+
+	// dirLOL, err := ioutil.ReadDir(".")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for key, value := range dirLOL {
+	// 	fmt.Println(key, value.Name(), value.Size())
+	// }
+
+}
